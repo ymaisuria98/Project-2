b61b2ce80e346bc6539bbc7922fb2033d9fd8a51
ymaisuria98 <123705276+ymaisuria98@users.noreply.github.com>
2023-05-01 16:52:41 -0400

    Stats Library

---
+++ Proj2/app/src/main/java/proj2/StatsLibrary.java
@@ -0,0 +1,226 @@
+package proj2;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
+
+public class StatsLibrary {
+
+	public double findMean(int[] oddList) {
+		// Calculates the mean (average) of the given list
+		double sum = 0;
+		for (double singleElement : oddList) {
+			sum = sum + singleElement;			
+		}
+		double result = sum/ oddList.length;
+		return result;
+	}
+	
+	
+	public double findMedian (int[] list) {
+		// Calculates the median of the given list
+		int n=list.length;
+		double median= 0.0;
+		Arrays.sort(list);
+		if((n/2)%2==0)
+		{
+			median=(list[n/2] + list[n/2+1])/2;
+		}
+		else
+		{
+			median=list[n/2+1];
+		}
+		return median;
+	}
+
+	
+	public double findMode (int[] list) {
+		// Calculates the mode (most frequent value) of the given list
+		int value=0, mcount=0;
+		for (int i = 0; i < list.length; ++i) {
+			int count = 0;
+			for (int j = 0; j < list.length; ++j) {
+				if(list[j] == list[i]) ++count;
+			}
+			if (count > mcount) {
+				mcount = count;
+				value = list[i];
+			}
+		}
+		return value;
+	}
+	public double standardDeviation (int[] evenList) {
+		// Calculates the standard deviation of the given list
+		double sum = 0;	
+		double squareSum = 0;
+		for (int i=0;i<evenList.length;i++) {
+			sum = sum + evenList[i];
+		}
+			double average = sum/evenList.length;
+		for (int i=0;i<evenList.length;i++) {
+			squareSum = squareSum + (evenList[i]-average)*(evenList[i]-average);
+		}
+		return Math.sqrt(squareSum/(evenList.length-1));
+		}
+	
+	public int factorial(int n){
+	// Calculates the factorial of a number
+	int result = 1;
+	if (n < 0) {
+	return 0;
+	}
+	while (n > 1) {
+	result *= n;
+	n--;
+	}
+	return result;
+	}
+
+	public int permutation(int n, int r){
+		// Calculates the permutation of n choose r
+		return factorial(n)/factorial(n-r);
+	}
+	
+	public int combinations(int n, int r) {
+		// Calculates the combinations of n choose r
+	    if(r == 0 || r == n)
+	        return 1;
+	    return combinations(n-1, r-1) + combinations(n-1, r);
+	}
+	
+	
+	public <T> List<T> union(List<T> list1, List<T> list2) {
+		// Returns the union of two lists (removes duplicates)
+		Set<T> set = new HashSet<T>();
+
+	        set.addAll(list1);
+	        set.addAll(list2);
+
+	        return new ArrayList<T>(set);
+	    }
+
+	    public <T> List<T> intersection(List<T> list1, List<T> list2) {
+	    	// Returns the intersection of two lists (common elements
+	    	List<T> list = new ArrayList<T>();
+
+	        for (T t : list1) {
+	            if(list2.contains(t)) {
+	                list.add(t);
+	            }
+	        }
+
+	        return list;
+	    }
+	    public <T> List <T> complement(List<T> list1, List<T> list2) {
+	    	// Returns the complement of list1 with respect to list2 (elements in list1 but not in list2)
+	    	Set<T> set = new HashSet<T>();
+
+	        set.addAll(list1);
+	        set.addAll(list2);
+	        for (T t : list1) {
+	            if(list2.contains(t)) {
+	                set.remove(t);
+	            }
+	        }
+	        return new ArrayList<T>(set);
+	    }        
+	    
+	    public double binomial(int p, int y, double prob){
+	    	// Calculates the probability of y successes in p trials with a given probability of success (binomial distribution)
+	        double way= combinations(p,y);
+	        double prob_each_way = Math.pow(prob,y)*Math.pow((1.0-prob),p-y);
+	        return (way*prob_each_way);
+	    }
+	   
+	    public double geometric (int p, double prob) {
+	    	// Calculates the probability of getting a success before p failures (geometric distribution)
+	    	return 1-Math.pow(1 - prob, p);
+    }
+	    
+	   
+	    static Random random = new Random();
+
+	    public int montyHall(boolean playerSwitch) {
+	    	// Simulates the Monty Hall problem where a player can choose to switch or not switch doors
+	        if (playerSwitch) {
+
+	        	int prizeDoor = random.nextInt(3);
+	            int playerChoose = random.nextInt(3);
+	            int wrong_door = prizeDoor;
+	            while (wrong_door == prizeDoor ||
+	                   wrong_door == playerChoose) {
+	                wrong_door = random.nextInt(3);
+	            }
+
+	            int alternate_door = 3 - (playerChoose + wrong_door);
+
+	            if (alternate_door == prizeDoor) return 1;
+	            else return 0;
+
+	        } else {
+
+	        	int prizeDoor = random.nextInt(3);
+	            int playerChoose = random.nextInt(3);
+	            if (prizeDoor == playerChoose) return 1;
+	            else return 0;
+	        }
+
+	        
+	    }
+	    public boolean birthday (int sampleSize) {
+	    	// Simulates the birthday problem where it checks if there is a matching birthday in a sample of a given size
+	    	ThreadLocalRandom rand = ThreadLocalRandom.current();
+			int[] days = new int [365];
+			
+			for (int i = 0; i < sampleSize; i++) {
+				int j = rand.nextInt(days.length);
+				days[j]++;
+				if (days[j] == 2) {
+					return true;
+				}
+			}
+			return false;
+	    }
+	    
+	    public double poisson(int x, double lambda) {
+	    	// Calculates the probability of x occurrences in a given interval with a given average rate (Poisson distribution)
+	    	return (Math.exp(-lambda) * Math.pow(lambda, x)) / factorial(x);
+	    }
+
+	    public double hypergeometric(int K, int n, int k, int x) {
+	    	// Calculates the probability of x successes in a sample without replacement from a finite population (hypergeometric distribution)
+	    	double numerator = combinations(K, x) * combinations(n - K, k - x);
+	        double denominator = combinations(n, k);
+	        return numerator / denominator;  
+	    }
+	    /**
+	     * Calculates the range of values within k standard deviations from the mean using Chebyshev's Theorem.
+	     *
+	     * data       an array of data points
+	     * k          the number of standard deviations from the mean
+	     * the range of values within k standard deviations from the mean
+	     */
+	    public double chebyshevRange(int[] data, int k) {
+	        int mean = (int) findMean(data);
+	        int stdDev = (int) standardDeviation(data);
+
+	        int lowerBound = mean - (k * stdDev);
+	        int upperBound = mean + (k * stdDev);
+
+	        return upperBound - lowerBound;
+	    }
+
+
+	    }
+
+
+	    
+
+
+
+
---
+++ Proj2/app/src/test/java/proj2/TestStatsLibrary.java
@@ -0,0 +1,144 @@
+package proj2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+   *
+   * Author: Yash Maisuria
+   * Date: 1/26/23
+   *
+*/
+
+public class TestStatsLibrary {
+	/**
+	   * Main method
+	*/
+	public static void main (String[] args) { 
+		StatsLibrary test = new StatsLibrary();
+		// Example of finding the mean of an array of numbers
+		int[] someNumbers = {1,2,3,4};
+		 // 1 + 2 + 3 + 4 = 10/4 = Expected result 2.5
+		double result = test.findMean(someNumbers);
+		System.out.println("Average of input " + result);
+	
+		// Example of using various statistical functions on evenList and oddList arrays
+		int[] evenList = { 38, 56, 18, 20, 12, 44 };
+		int[] oddList = { 5, 21, 49, 41, 31, 19 };
+
+		System.out.print("Even List: ");
+		for (int i = 0; i < evenList.length; i++)
+			System.out.print(evenList[i] + " ");
+		System.out.println("\nMean: " + (test.findMean(evenList)));
+		System.out.println("Median: " + (test.findMedian(evenList)));
+		System.out.println("Mode: " + test.findMode(evenList));
+		System.out.println("Standard Deviation: " + test.standardDeviation(evenList));
+
+		System.out.print("\nOdd List: ");
+		for (int i = 0; i < oddList.length; i++)
+			System.out.print(oddList[i] + " ");
+		System.out.println("\nMean: " + (test.findMean(oddList)));
+		System.out.println("Median: " + (test.findMedian(oddList)));
+		System.out.println("Mode: " + test.findMode(oddList));
+		System.out.println("Standard Deviation: " + test.standardDeviation(oddList));
+
+		// Example of combining two arrays into a single array and performing statistical calculations on it
+		int[] wholeList = new int[evenList.length + oddList.length];
+		System.arraycopy(evenList, 0, wholeList, 0, evenList.length);
+		System.arraycopy(oddList, 0, wholeList, evenList.length, oddList.length);
+
+		System.out.print("\nWhole List: ");
+		for (int i = 0; i < wholeList.length; i++)
+			System.out.print(wholeList[i] + " ");
+		System.out.println("\nMean: " + (test.findMean(wholeList)));
+		System.out.println("Median: " + (test.findMedian(wholeList)));
+		System.out.println("Mode: " + test.findMode(wholeList));
+		System.out.println("Standard Deviation: " + test.standardDeviation(wholeList));
+
+		// Example of using permutation and combination functions
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("\nEnter Total Amount In Set: ");
+		int n = scanner.nextInt();
+		System.out.print("Enter Amount in each Sub-Set: ");
+		int r = scanner.nextInt();
+		System.out.println("Permutation: (" + n + ", " + r + ") = " + test.permutation(n, r));
+		System.out.println("Combinations: (" + n + ", " + r + ") = " + test.combinations(n, r));
+
+		// Example of using set operations on two lists
+		List<String> list1 = new ArrayList<String>(Arrays.asList("A", "B", "C"));
+		List<String> list2 = new ArrayList<String>(Arrays.asList("B", "C", "D", "E", "F"));
+
+		System.out.println("\nList 1:" + list1);
+		System.out.println("List 2:" + list2);
+		System.out.println("Intersection: " + new StatsLibrary().intersection(list1, list2));
+		System.out.println("Union: " + new StatsLibrary().union(list1, list2));
+		System.out.println("Complement: " + new StatsLibrary().complement(list1, list2));
+
+		// Example of using probability and statistical distributions (Binomial and Geometric)
+		Scanner scanner1 = new Scanner(System.in);
+		System.out.print("\nEnter Number of Trials: ");
+		int p = scanner.nextInt();
+		System.out.print("Enter Successes: ");
+		int y = scanner.nextInt();
+		System.out.print("Enter Probability of Success: ");
+		double prob = scanner.nextDouble();
+		System.out.println("Binomial: (" + p + ", " + y + ", " + prob + ") = " + test.binomial(p, y, prob));
+		System.out.println("Geometric: (X < " + p + ") = " + test.geometric(p, prob));
+
+		// Example of simulating Monty Hall problem and birthday paradox
+		int times = 100000;
+		int winsWithNoSwitch = 0, winsWithSwitch = 0;
+		for (int x = 1; x <= times; x++) {
+		    winsWithNoSwitch += test.montyHall(false);
+		    winsWithSwitch += test.montyHall(true);
+		}
+
+		System.out.println("\nWin Chance With Switching: " + winsWithSwitch * 100.0 / times);
+		System.out.println("Win Chance Without Switching: " + winsWithNoSwitch * 100.0 / times);
+
+		int numPeople = 30;
+		int trial = 1000000;
+		int match = 0;
+		for (int i = 0; i < trial; i++) {
+		    if (test.birthday(numPeople)) {
+		        match++;
+		    }
+		}
+		System.out.println("\nSampling Birthdays Number Of People: " + numPeople);
+		double probability = (double) match / trial * 100;
+		System.out.println("Probability Of A Birthday Match: " + probability);
+
+		// Example of using Poisson and Hypergeometric distributions
+		int poissonX = 2; // Number of events
+		double poissonLambda = 1.5; // Mean
+		double poissonResult = test.poisson(poissonX, poissonLambda);
+		System.out.println("Poisson Distribution:");
+		System.out.println("P(X = " + poissonX + " | lambda = " + poissonLambda + ") = " + poissonResult);
+
+		int hypergeometricK = 5; // Number of successes in the population
+		int hypergeometricN = 10; // Population size
+		int hypergeometricKSample = 3; // Sample size
+		int hypergeometricX = 2; // Number of successes in the sample
+		double hypergeometricResult = test.hypergeometric(hypergeometricK, hypergeometricN, hypergeometricKSample, hypergeometricX);
+		System.out.println("Hypergeometric Distribution:");
+		System.out.println("P(X = " + hypergeometricX + " | K = " + hypergeometricK + ", N = " + hypergeometricN +
+		        ", k = " + hypergeometricKSample + ") = " + hypergeometricResult);
+		
+		// Define an array of data points
+		int[] data = {10, 12, 14, 16, 18, 20, 22};
+
+		// Specify the number of standard deviations from the mean
+		int k = 2;
+
+		// Calculate the range of values within k standard deviations from the mean using Chebyshev's Theorem
+		double range = test.chebyshevRange(data, k);
+
+		// Print the result
+		System.out.println("Chebyshev's Theorem:");
+		System.out.println("Range within " + k + " standard deviations: " + range);
+
+	}
+}
+    
